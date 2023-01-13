69ce2fd91bbf4fcb5b9c9e84fd2ca37b92eb22db
Tomoyuki Fujii <mjr01300828@gmail.com>
2023-01-12 19:57:48 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Find from "./components/find/Find";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/nabvar/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Find />
+    </div>
+  );
+}
+
+export default App;
