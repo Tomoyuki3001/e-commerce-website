fee038c98d9acfdf2caf7a711f88b895eda14f85
Tomoyuki Fujii <mjr01300828@gmail.com>
2023-01-13 18:59:18 -0800

    Added some changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import Driver from "./components/driver/Driver";
 import Find from "./components/find/Find";
+import Footer from "./components/footer/Footer";
 import Hero from "./components/hero/Hero";
+import Luxury from "./components/luxury/Luxury";
 import Navbar from "./components/nabvar/Navbar";
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
       <Navbar />
       <Hero />
       <Find />
+      <Driver />
+      <Luxury />
+      <Footer />
     </div>
   );
 }
